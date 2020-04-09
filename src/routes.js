09b60caa9d436a44025c2daf05bc9fbe74cfe525
mbssilva <mbernardi.s@outlook.com>
2020-04-09 20:34:05 -0300

    Estrutura inicial do projeto. Background quebrando a aplicação

---
+++ src/routes.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+// import {} from '@react-navigat';
+
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+// import Header from './components/Header';
+
+const Stack = createStackNavigator();
+
+function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        headerMode="none"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Routes;
